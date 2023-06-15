a38cf99feef17427399328c5b62474a5f3569926
TarasVarvarych <varvarychtaras@gmail.com>
2023-06-15 21:21:56 +0300

    123

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://taras:rk9c3d3XrHJa6EwC@contacts.fe1scp4.mongodb.net/";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
