7854def4c7e374f700e004655bf0cb56d729a824
TarasVarvarych <varvarychtaras@gmail.com>
2023-06-29 17:40:40 +0300

    123

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,14 +6,15 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401));
   }
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token || user.token !== token) {
+    if (!user.token) {
       next(HttpError(401));
     }
     req.user = user;
